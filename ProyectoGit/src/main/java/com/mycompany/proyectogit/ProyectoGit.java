51142b742b068968b28787ff9dfd16015e2bce31
Yendy Leon <yendylt@gmail.com>
2024-01-10 12:52:28 +0100

    add Proyecto Hola Mundo

---
+++ ProyectoGit/src/main/java/com/mycompany/proyectogit/ProyectoGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectogit;
+
+/**
+ *
+ * @author Yen
+ */
+public class ProyectoGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
